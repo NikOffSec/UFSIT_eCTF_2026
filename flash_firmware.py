c0b76ec9455efa896a9861b8747c3a2b451cb39b
Raspberry Pi Server <colesmith1@ufl.edu>
2026-02-11 18:53:07 -0500

    better log msgs

--- flash_firmware.py
+++ flash_firmware.py
@@ -14,41 +14,41 @@ SW_PIN = 27
 GPIO.setup(RST_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(SW_PIN, GPIO.OUT, initial=GPIO.LOW)
 
-print("LED blinking started. Press Ctrl+C to stop.")
-
+print("Starting flash process!!! Do not stop early let it finish completely!!!")
 
 try:
-    print("Pulling RESET LOW")
+    print("--- Putting Board in Flash State ---")
+    #print("Pulling RESET LOW")
     GPIO.output(RST_PIN, GPIO.HIGH)
     
     sleep(1)
     
-    print("Pulling Flash Switch LOW")
+    #print("Pulling Flash Switch LOW")
     GPIO.output(SW_PIN, GPIO.HIGH)
     
     sleep(1)
     
-    print("Pulling RESET HIGH")
+    #print("Pulling RESET HIGH")
     GPIO.output(RST_PIN, GPIO.LOW)
 
     sleep(1)
     
-    print("Pulling Flash Switch RESET HIGH")
+    #print("Pulling Flash Switch RESET HIGH")
     GPIO.output(SW_PIN, GPIO.LOW)
-    print("Running UVX command right now to flash board....")
+    print("--- Running UVX commands right now to flash board ---")
     sleep(1)
     #os.system("./uvx_flash.sh")
-    print("Erasing Existing Firmware")
+    print("--- Erasing Existing Firmware ---")
     os.system("uvx ectf hw /dev/ttyACM0 erase")
     sleep(3)
-    print("Flashing New Firmware")
+    print("--- Flashing New Firmware ---")
     os.system("uvx ectf hw /dev/ttyACM0 flash ./build/hsm.bin -n hsm")
     sleep(3)
-    print("Sending Start Command")
+    print("--- Sending Start Command ---")
     os.system("uvx ectf hw /dev/ttyACM0 start")
 
 except KeyboardInterrupt:
-    print("Issue turning on GPIO.")
+    print("!!!FLASH FAILED PING COLE TO FIX!!!")
     GPIO.cleanup()
 
 GPIO.cleanup()
