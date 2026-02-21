3f0b9b98f0f6dd82c78ac15015a7b027445167d4
Admin <admin@Cushman-MacBook-Air.local>
2026-02-21 16:53:56 -0500

    Need to test if power required

--- firmware/src/simple_timer.c
+++ firmware/src/simple_timer.c
@@ -6,17 +6,18 @@ int timer_init() {
 
     print_debug("[DEBUG] Awakening the Counter module.");
 
+    if (!(DL_Timer_isPowerEnabled(TIMER_0_INST))) {
+        return -1;
+    }
+
     DL_TimerG_setClockConfig(TIMER_0_INST, (DL_TimerG_ClockConfig*)&gTIMER_0ClockConfig);
     DL_TimerG_initTimerMode(TIMER_0_INST, (DL_TimerG_TimerConfig*)&gTIMER_0TimerConfig);
     DL_TimerG_enableInterrupt(TIMER_0_INST , DL_TIMERG_INTERRUPT_ZERO_EVENT);
     DL_TimerG_enableClock(TIMER_0_INST);
-    DL_TimerG_startCounter(TIMER_0_INST);
 
     print_debug("Waiting for initial Timer setup");
-    STATUS_LED_ON();
-    STATUS_LED_OFF();
+    DL_TimerG_startCounter(TIMER_0_INST);
     while(DL_Timer_isRunning(TIMER_0_INST))
-    STATUS_LED_ON();
     print_debug("Timer Finished!");
 
     return 0;
