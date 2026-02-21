1630634f3b25361e8892320d86a3ad0859504025
Admin <admin@Cushman-MacBook-Air.local>
2026-02-21 17:02:20 -0500

    Added poweron code for timer

--- firmware/src/simple_timer.c
+++ firmware/src/simple_timer.c
@@ -4,9 +4,15 @@
 
 int timer_init() {
 
-    print_debug("[DEBUG] Awakening the Counter module.");
+    print_debug("[DEBUG] Awakening the Timer module.");
+
+    SYSCFG_DL_initPower();
+    DL_TimerG_reset(TIMER_0_INST);
+    DL_TimerG_enablePower(TIMER_0_INST);
+    delay_cycles(POWER_STARTUP_DELAY);
 
     if (!(DL_Timer_isPowerEnabled(TIMER_0_INST))) {
+        print_debug("[DEBUG] Timer power not working. Abort");
         return -1;
     }
 
